ecf7544f5d706ded7c645a03b530ec279a5351e3
Tim McMullin <timothymcmullin@gmail.com>
2013-12-02 14:51:19 -0600

    Added missing comma

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ var app = {
     renderHomeView: function() {
     	$('body').html(this.homeTpl());
     	$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	}
+	},
 	//end of renderHomeView function
 
     initialize: function() {
