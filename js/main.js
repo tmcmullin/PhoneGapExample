34e234e7287e2b8f6c4a0a5a620d13adecda151d
Tim McMullin <timothymcmullin@gmail.com>
2013-12-02 14:01:03 -0600

    Add alerts to application

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+	showAlert: function (message, title) {
+    	if (navigator.notification) {
+	        navigator.notification.alert(message, null, title, 'OK');
+	    } else {
+        	alert(title ? (title + ": " + message) : message);
+    	}
+	},
+	//end of showAlert function
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    findByName: function() {
+    	var self = this;
+    	this.store.findByName($('.search-key').val(), function(employees) {
+	        $('.employee-list').html(self.employeeLiTpl(employees));
+    	});
+	},
+    //end of findByName function
+
+	initialize: function() {
+    	this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+    	var self = this;
+    	this.store = new MemoryStore(function() {
+	        self.renderHomeView();
+	    });
+	},
+    //end of initialize function
+    
+    renderHomeView: function() {
+    	$('body').html(this.homeTpl());
+    	$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	}
+	//end of renderHomeView function
 
 };
 
